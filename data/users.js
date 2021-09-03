6af333add7e1abde4aadcce34bf6ea0acd811ff5
nicole-lapidus <nlapidus@udesa.edu.ar>
2021-09-03 14:37:44 -0300

    Usuarios

--- data/users.js
+++ data/users.js
@@ -1,5 +1,48 @@
 var users = {
-
+    list: [
+        {
+            name: 'Nicole',
+            surname: 'Lapidus',
+            biography: '',
+            level: 'Experto',
+            posts: '100'
+        },
+        {
+            name: 'Tatiana',
+            surname: 'Katz',
+            biography: '',
+            level: 'Avanzado',
+            posts: '203'
+        },
+        {
+            name: 'Carolina',
+            surname: 'Camblor',
+            biography: '',
+            level: 'Principiante',
+            posts: '55'
+        },
+        {
+            name: 'Julian',
+            surname: 'Gomez',
+            biography: '',
+            level: 'Experto',
+            posts: '3'
+        },
+        {
+            name: 'Jazmin',
+            surname: 'Lopez',
+            biography: '',
+            level: 'Principiante',
+            posts: '320'
+        },
+        {
+            name: 'Nicolas',
+            surname: 'Rodriguez',
+            biography: '',
+            level: 'Avanzado',
+            posts: '49'
+        }
+    ]
 }
 
 module.exports = users;
