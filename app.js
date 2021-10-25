b26ac26eb0909f038cd751f7a1cf2877a97fa169
Tatiana <69600097+TatiKatz@users.noreply.github.com>
2021-10-25 16:14:38 -0300

    app

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var postsRouter = require('./routes/posts');
 
 var app = express();
+
+app.use( //configuracion de session. Nos agreega la variable req.session
+  session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7, //da 7 dias y 1.000 segundos
+    },
+  }),
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +32,13 @@ app.use(express.json()); //se fija lo que viene en la request y la pasa json
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(async (req, res, next) => { //Middleware. Poner en vistas
+  if (req.session.user !== undefined) {
+    res.locals.user = req.session.user; //res.locals es varible que se comparte con las vistas 
+  }
+  next();
+}); //se ejecuta siempre a menos que uno de una respuesta
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
