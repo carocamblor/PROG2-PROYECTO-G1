643f58ad0334b2d7abb0e29adec8769ea45da6d1
nicole-lapidus <nlapidus@udesa.edu.ar>
2021-10-28 16:46:29 -0300

    Configuracion de multer

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const multer = require ('multer');
+const upload = multer ({dest: 'public/images/'})
 var postsController = require('../controllers/postsController');
 
-router.post('/publish', postsController.store);
+//router.get ('/publish', postsController.publish); no esta en controlador
+
+router.post('/publish', upload.single('image') , postsController.store);
 
 router.get('/newpost', postsController.newPost);
 
